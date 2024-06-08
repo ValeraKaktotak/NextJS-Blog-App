11510ef473af2265533af4b6be9bc288aeb2d645
Valera <valerakaktotak@gmail.com>
2024-06-08 23:57:00 +0300

    added media and few main styles in global css file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,59 @@
-import styles from "./homepage.module.css";
+import Link from 'next/link'
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Link href='/'>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        temporibus nihil dolores ipsum consectetur voluptate enim, eius omnis
+        quibusdam laboriosam ad, veniam nobis sequi exercitationem excepturi et
+        culpa consequatur minus?
+      </h1>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam non
+      impedit inventore minima adipisci necessitatibus odit accusantium
+      doloremque. Vel maxime sunt beatae similique commodi. Non atque quisquam
+    </Link>
+  )
 }
