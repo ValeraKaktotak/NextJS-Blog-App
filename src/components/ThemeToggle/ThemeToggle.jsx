e5f55497fbe9a9eb98acc5faeb9b6800fc2000f0
Valera <valerakaktotak@gmail.com>
2024-06-11 20:26:40 +0300

    finished with theme toggle switcher

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -11,7 +11,14 @@ const ThemeToggle = () => {
   return (
     <div className={styles.container} onClick={toggle}>
       <Image src='/moon.png' alt='moon' width={14} height={14} />
-      <div className={styles.ball}></div>
+      <div
+        className={styles.ball}
+        style={
+          theme === 'dark'
+            ? { left: 1, background: '#0f172a' }
+            : { right: 1, background: 'white' }
+        }
+      ></div>
       <Image src='/sun.png' alt='sun' width={14} height={14} />
     </div>
   )
