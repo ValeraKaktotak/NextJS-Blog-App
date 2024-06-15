6868199f15f12d1b084f4777e0c4f4ab1a674f3a
Valera <valerakaktotak@gmail.com>
2024-06-15 14:22:05 +0300

    done google and github OAuth providers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer/Footer'
 import Navbar from '@/components/Navbar/Navbar'
 import { ThemeContextProvider } from '@/context/ThemeContext'
+import AuthProvider from '@/providers/AuthProvider'
 import ThemeProvider from '@/providers/ThemeProvider'
 import { Inter } from 'next/font/google'
 import { cookies } from 'next/headers'
@@ -20,17 +21,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ThemeContextProvider themeFromCookies={theme}>
-          <ThemeProvider>
-            <div className='container'>
-              <div className='wrapper'>
-                <Navbar />
-                {children}
-                <Footer />
+        <AuthProvider>
+          <ThemeContextProvider themeFromCookies={theme}>
+            <ThemeProvider>
+              <div className='container'>
+                <div className='wrapper'>
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </ThemeContextProvider>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   )
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,30 @@
+'use client'
+import { signIn, useSession } from 'next-auth/react'
 import styles from './Login.module.css'
 
 const LoginPage = () => {
+  const { data, status } = useSession()
+  console.log(data)
+  console.log(status)
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
-        <div className={styles.socialButton}>Sign in with GitHub</div>
+        <div
+          className={styles.socialButton}
+          onClick={() => {
+            signIn('google')
+          }}
+        >
+          Sign in with Google
+        </div>
+        <div
+          className={styles.socialButton}
+          onClick={() => {
+            signIn('github')
+          }}
+        >
+          Sign in with GitHub
+        </div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
     </div>
