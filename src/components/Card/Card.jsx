cb1579d167f965404448c5f1ac2644a41605ab06
Valera <valerakaktotak@gmail.com>
2024-06-18 00:42:34 +0300

    replaced route to single post page and add simple changed in Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './Card.module.css'
 
-const Card = ({ key, item }) => {
+const Card = ({ item }) => {
   return (
-    <div key={key} className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+        {item.img && (
+          <Image src={item.img} alt='' fill className={styles.image} />
+        )}
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
@@ -15,16 +17,11 @@ const Card = ({ key, item }) => {
           </span>
           <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href='/'>
+        <Link href={`/post/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-          quae laudantium suscipit ex debitis? Temporibus porro nulla eum in!
-          Nostrum sit facilis, quos ullam nihil reprehenderit aperiam fugit
-          inventore similique!
-        </p>
-        <Link href='/' className={styles.link}>
+        <p className={styles.desc}>{item.desc.substring(0, 100)}...</p>
+        <Link href={`/post/${item.slug}`} className={styles.link}>
           Read More
         </Link>
       </div>
