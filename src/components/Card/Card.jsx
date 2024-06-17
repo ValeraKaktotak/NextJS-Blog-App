b6988017511285928ba6b203ad4efc26fcb675f6
Valera <valerakaktotak@gmail.com>
2024-06-17 23:28:42 +0300

    finished with prisma schemas and create categories and posta routes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './Card.module.css'
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div key={key} className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src='/p1.jpeg' alt='' fill className={styles.image} />
       </div>
@@ -14,7 +14,7 @@ const Card = () => {
           <span className={styles.category}>CULTURE</span>
         </div>
         <Link href='/'>
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
         <p className={styles.desc}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
